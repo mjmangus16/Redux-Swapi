a801ab458c28706cfcd013f1897fba74e76112f3
MMangus <mjmangus16@yahoo.com>
2019-06-19 17:00:28 -0400

    completed MVP

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,12 @@
-import /* we need our action types here*/ "../actions";
+import {
+  GET_CHARACTERS_SUCCESS,
+  FETCHING,
+  GET_CHARACTERS_FAILURE
+} from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: true,
+  error: ""
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +14,21 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true
+      };
+    case GET_CHARACTERS_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        characters: action.payload
+      };
+    case GET_CHARACTERS_FAILURE:
+      return {
+        ...state
+      };
     default:
       return state;
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getCharacters } from "../actions/index";
 
 import { CharacterList } from "../components";
 // import actions
@@ -10,26 +11,40 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      console.log(this.props.fetching);
+      return <div>FETCHING DATA...</div>;
     }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
+    console.log(this.props.characters);
+    {
+      if (this.props.characters.results.length > 0) {
+        return (
+          <div className="CharactersList_wrapper">
+            <CharacterList characters={this.props.characters.results} />
+          </div>
+        );
+      }
+    }
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching
+  };
+};
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
